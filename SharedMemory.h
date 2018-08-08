19c5562744f068661d4ba4367d51b3001ff06f4a
Jinming YAO <cwlmyjm@126.com>
2018-08-08 10:20:19 +0800

    Add permission control
    Add permission control and remove file create operation

--- SharedMemory.h
+++ SharedMemory.h
@@ -1,59 +1,51 @@
 ﻿#pragma once
 #include <Windows.h>
 
+enum Permission {
+	CREATE_RW,
+	CREATE_R,
+	OPEN_RW,
+	OPEN_R,
+};
+
 template<typename T>
 class SharedMemory
 {
 public:
-	SharedMemory(const char* s_name, bool s_writeable);
+	SharedMemory(const char* s_name, Permission s_permission);
 	~SharedMemory();
 
 	bool read(T* output);
 	bool write(T* input);
 private:
 	bool s_writeable;
-	HANDLE s_fileHandle;
 	HANDLE s_mapHandle;
 	T* s_sharedData;
 };
 
 template<typename T>
-SharedMemory<T>::SharedMemory(const char* s_name, bool s_writeable) : s_writeable(s_writeable)
+SharedMemory<T>::SharedMemory(const char* s_name, Permission s_permission)
+	: s_writeable(s_permission == Create_RW || s_permission == Open_RW)
 {
-	if (s_writeable){
-		s_fileHandle = CreateFile(s_name, GENERIC_READ | GENERIC_WRITE,
-			0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-
-		if (s_fileHandle == nullptr) {
-			throw - 1;
-		}
-
-		s_mapHandle = CreateFileMapping(s_fileHandle, NULL, PAGE_READWRITE, 0, sizeof(T), s_name);
-
-		if (s_mapHandle == nullptr) {
-			throw - 2;
-		}
-
-		s_sharedData = (T*)MapViewOfFile(s_mapHandle, FILE_MAP_WRITE, 0, 0, sizeof(T));
-
-		if (s_sharedData == nullptr) {
-			throw - 3;
-		}
+	if (s_permission == Create_RW || s_permission == Create_R) {
+		s_mapHandle = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, sizeof(T), s_name);
 	}
-	else{
-		s_mapHandle = OpenFileMapping(FILE_MAP_READ, FALSE, s_name);
+	else {
+		s_mapHandle = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, s_name);
+	}
+	if (s_mapHandle == nullptr) {
+		throw - 2;
+	}
 
-		if (s_mapHandle == nullptr) {
-			throw - 1;
-		}
-
+	if (s_writeable) {
+		s_sharedData = (T*)MapViewOfFile(s_mapHandle, FILE_MAP_ALL_ACCESS, 0, 0, sizeof(T));
+	}
+	else {
 		s_sharedData = (T*)MapViewOfFile(s_mapHandle, FILE_MAP_READ, 0, 0, sizeof(T));
-
-		if (s_sharedData == nullptr) {
-			throw - 2;
-		}
 	}
-
+	if (s_sharedData == nullptr) {
+		throw - 3;
+	}
 };
 
 template<typename T>
@@ -64,14 +56,10 @@ SharedMemory<T>::~SharedMemory()
 
 	// 关闭对象句柄
 	if (s_mapHandle != nullptr) CloseHandle(s_mapHandle);
-
-	// 关闭文件句柄
-	if (s_fileHandle != nullptr) CloseHandle(s_fileHandle);
-
 };
 
 template<typename T>
-bool SharedMemory<T>::read(T* output){
+bool SharedMemory<T>::read(T* output) {
 	if (s_sharedData == nullptr) {
 		return false;
 	}
@@ -82,7 +70,7 @@ bool SharedMemory<T>::read(T* output){
 }
 
 template<typename T>
-bool SharedMemory<T>::write(T* input){
+bool SharedMemory<T>::write(T* input) {
 	if (input == nullptr) {
 		return false;
 	}
